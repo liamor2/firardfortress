1ae0119ac01f57636af4dd6ebe02bff31aa31421
liamor2 <87279441+liamor2@users.noreply.github.com>
2023-11-27 00:16:27 +0100

    Oui

--- scripts/sheets/firardFortressActorSheets.js
+++ scripts/sheets/firardFortressActorSheets.js
@@ -1,7 +1,8 @@
+import { gsap } from "/scripts/greensock/esm/all.js";
+
 export default class firardFortressActorSheet extends ActorSheet {
     get template() {
         console.log(`firardFortress | Loading ${this.actor.type} sheet`);
-        // console.log(this.actor);
 
         return `systems/firardfortress/templates/sheets/actors/${this.actor.type}-sheets.hbs`;
     }
@@ -10,13 +11,14 @@ export default class firardFortressActorSheet extends ActorSheet {
         return mergeObject(super.defaultOptions, {
             width: 520,
             height: 480,
-            classes: ["firardFortress", "sheet", "item"]
+            classes: ["firardFortress", "sheet", "item"],
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main"}],
+            dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
         });
     }
 
-    async getData() {
+    getData() {
         const data = super.getData();
-        data.config = CONFIG.firardFortress;
         for (let [key, value] of Object.entries(data.data.system.stat)) {
             value.mod.num = Math.floor((value.value - 10) / 2);
             if (value.mod.num >= 0) {
@@ -32,6 +34,7 @@ export default class firardFortressActorSheet extends ActorSheet {
         super.activateListeners(html);
 
         html.find('.rollable').click(this._onRoll.bind(this));
+        html.find('.advancedRoll').click(this._onAdvancedRoll.bind(this));
     }
 
     _onRoll(event) {
@@ -40,13 +43,27 @@ export default class firardFortressActorSheet extends ActorSheet {
         const dataset = element.dataset;
 
         let roll = new Roll(dataset.roll, this.actor.getRollData());
-        console.log(roll);
         let label = dataset.label ? `Rolling ${dataset.label}` : '';
         return roll.toMessage({
             speaker: ChatMessage.getSpeaker({
                 actor: this.actor
             }),
             flavor: label
+        });
+    }
+    _onAdvancedRoll(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        const dataset = element.dataset;
+        const stat = dataset.roll;
+        const data = this.getData();
+        // console.log(data.data.system.stat[stat].value);
+
+        gsap.to(".advancedRoll", {
+            y: 200,
+            duration: 0.5,
+            opacity: 1,
+            display: "block"
         });
     }
 }
